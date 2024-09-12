7fd7d37bb674c291e9417c40d2bb85662645e6e3
Akshay Laxmipathi <52963912+Akshay-Laxmipathi@users.noreply.github.com>
2024-09-12 17:55:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(int i=0; i<str.length; i++) {
+		let char s = str.charAt(i);
+		for(int j=0; i<str.length; j++) {
+			if(i == j) {
+				continue;
+			}
+			if(s == str.charAt(j)) {
+				s = null;
+				break;
+			}
+		}
+	return s;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
